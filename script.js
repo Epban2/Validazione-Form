28cba002b154a621801cad9d6a97478eb507848f
Riccardo La Rocca <sriccardo.larocca@itis.pr.it>
2024-02-09 11:35:06 +0100

    piccole modifiche

--- script.js
+++ script.js
@@ -1,43 +1,41 @@
 function Modulo() {
-    var nome = document.modulo.nome.value;
-    var cognome = document.modulo.cognome.value;
-    var indirizzo = document.modulo.indirizzo.value;
-    var citta = document.modulo.citta.options[document.modulo.citta.selectedIndex].value;
-    var telefono = document.modulo.telefono.value;
-    var nascita = document.modulo.nascita.value;
+    var nome =document.getElementById("nome").value;
+    var cognome = document.getElementById("cognome").value;
+    var indirizzo = document.getElementById("indirizzo").value;
+    var citta = document.getElementById("citta").value;
+    var telefono =document.getElementById("telefono").value;
+    var nascita = document.getElementById("nascita").value;
 
     if ((nome == "") || (nome == "undefined")) {
-        alert("Il campo Nome è obbligatorio.");
-        document.modulo.nome.focus();
+        alert("Completare il campo nome.");
+        document.getElementById("nome").focus;
         return false;
     }
     else if ((cognome == "") || (cognome == "undefined")) {
-        alert("Il campo Cognome è obbligatorio.");
-        document.modulo.cognome.focus();
+        alert("Completare il campo cognome.");
+        document.getElementById("cognome").focus;
         return false;
     }
     else if (nascita == "") {
         alert("La data di nascita è obbligatoria");
-        document.modulo.nascita.value = "";
-        document.modulo.nascita.focus();
+        document.getElementById("nascita").focus;
         return false;
     }
     else if ((citta == "") || (citta == "undefined")) {
         alert("Il campo Città è obbligatorio.");
-        document.modulo.citta.focus();
+        document.getElementById("citta").focus;
         return false;
     }
     else if ((indirizzo == "") || (indirizzo == "undefined")) {
         alert("Il campo Indirizzo è obbligatorio.");
-        document.modulo.indirizzo.focus();
+        document.getElementById("indirizzo").focus;
         return false;
     }
     else if ((isNaN(telefono)) || (telefono == "") || (telefono == "undefined")) {
-        alert("Il campo Telefono è numerico ed obbligatorio.");
-        document.modulo.telefono.value = "";
-        document.modulo.telefono.focus();
+        document.getElementById("alert").innerHTML="Il campo Telefono è numerico ed obbligatorio.";
+        document.getElementById("telefono").focus;
         return false;
     }
-
     document.modulo.submit();
+    
 }
