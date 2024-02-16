f8ded73f73ad13fb5ac9b6ec61333506399237a1
Riccardo La Rocca <sriccardo.larocca@itis.pr.it>
2024-02-16 12:52:22 +0100

    Aggiunte regex

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 function Modulo() {
-    var nome =document.getElementById("nome").value;
+    var nome = document.getElementById("nome").value;
     var cognome = document.getElementById("cognome").value;
     var indirizzo = document.getElementById("indirizzo").value;
     var citta = document.getElementById("citta").value;
-    var telefono =document.getElementById("telefono").value;
+    var telefono = document.getElementById("telefono").value;
     var nascita = document.getElementById("nascita").value;
 
-    if ((nome == "") || (nome == "undefined")) {
-        alert("Completare il campo nome.");
+    var regexNome = /^[a-zA-Z]+$/; //solamente lettere maiuscole e minuscole
+    var regexCognome = /^[A-Za-z\s]+$/; //a differenza del nome contiene \s (spazio)
+    var regexIndirizzo = /^[a-zA-Z0-9\s]+$/; //lettere, numeri, spazi (no punteggiatura)
+    var regexTelefono = /^\d{10}$/; // solamente numeri, 10 cifre precise
+
+
+    if (!regexNome.test(nome)) {
+        document.getElementById("alert").innerHTML = "Correggere il campo nome!";
         document.getElementById("nome").focus;
         return false;
     }
-    else if ((cognome == "") || (cognome == "undefined")) {
-        alert("Completare il campo cognome.");
+    else if (!regexCognome.test(cognome)) {
+        document.getElementById("alert").innerHTML = "Correggere il campo cognome!";
         document.getElementById("cognome").focus;
         return false;
     }
     else if (nascita == "") {
-        alert("La data di nascita è obbligatoria");
+        document.getElementById("alert").innerHTML = "Correggere il campo nome!";
         document.getElementById("nascita").focus;
         return false;
     }
-    else if ((citta == "") || (citta == "undefined")) {
-        alert("Il campo Città è obbligatorio.");
-        document.getElementById("citta").focus;
-        return false;
-    }
-    else if ((indirizzo == "") || (indirizzo == "undefined")) {
-        alert("Il campo Indirizzo è obbligatorio.");
+    else if (!regexIndirizzo.test(indirizzo)) {
+        document.getElementById("alert").innerHTML = "Correggere il campo indirizzo!";
         document.getElementById("indirizzo").focus;
         return false;
     }
-    else if ((isNaN(telefono)) || (telefono == "") || (telefono == "undefined")) {
-        document.getElementById("alert").innerHTML="Il campo Telefono è numerico ed obbligatorio.";
-        document.getElementById("telefono").focus;
+    else if ((citta == "") || (citta == "undefined")) {
+        document.getElementById("alert").innerHTML = "Il campo città è obbligatorio!";
+        document.getElementById("citta").focus;
         return false;
     }
+
+    else if (!regexTelefono.test(telefono)) {
+        document.getElementById("alert").innerHTML = "Correggere il numero di telefono (10 cifre)";
+        document.getElementById("telefono").focus;
+        return false;
+    } else
+        document.getElementById("alert").innerHTML = "I campi sono validi";
     document.modulo.submit();
-    
+
 }
